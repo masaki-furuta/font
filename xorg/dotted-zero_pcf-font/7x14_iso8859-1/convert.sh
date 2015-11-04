82fb190dd5532c2520d27b8d62874502c4969ed6
Masaki Furuta <masaki.furuta@gmail.com>
2015-11-05 00:55:31 +0900

    Moved dirs.
    Signed-off-by: Masaki Furuta <masaki.furuta@gmail.com>

---
+++ xorg/dotted-zero_pcf-font/7x14_iso8859-1/convert.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+FNT=7x14-ISO8859-1
+FDR=/usr/share/X11/fonts/misc
+
+cp -v ${FDR}/${FNT}.pcf.gz .
+gunzip ${FNT}.pcf.gz
+pcf2bdf -o ${FNT}.bdf ${FNT}.pcf
+gbdfed ${FNT}.bdf
+bdftopcf -o ${FNT}.pcf ${FNT}.bdf
+gzip ${FNT}.pcf
+sudo mv -v ${FDR}/${FNT}.pcf.gz ${FDR}/${FNT}.pcf.gz-$(date +%F)
+sudo cp -v ${FNT}.pcf.gz ${FDR}/${FNT}.pcf.gz
+       
